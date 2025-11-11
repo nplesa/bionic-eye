f7821de373fa4b1c7c15b6501ec6fbc927349f8b
Nicolae Plesa <nicu.plesa@gmail.com>
2025-11-11 11:30:24 +0200

    Update BionicEyeServiceProvider.php

--- src/BionicEyeServiceProvider.php
+++ src/BionicEyeServiceProvider.php
@@ -7,7 +7,7 @@ class BionicEyeServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/Database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'bionic-eye');
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
